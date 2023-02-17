5641654cd9a8ddb752db699ac9c48e325ecd2bd2
macbookpro <juancaarboleda962008@hotmail.com>
2023-02-17 12:37:15 -0500

    Create project console .net core

---
+++ DesignedTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignedTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
